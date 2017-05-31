3ccd8333af2b7cfa173a5f93ec0a70a487a0dff2
Ryan Heller <ryanwheller@outlook.com>
2017-05-30 20:12:53 -0700

    structs in libTinyFS.h

---
+++ libTinyFS.h
@@ -0,0 +1,22 @@
+typedef struct superblock {
+   const int magic = 0x45; //detect when disk is not in correct format, second byte of every block
+   int inode_root; //block number of the root inode
+   struct free_block* free_head; //pointer to list of freeblock
+} superblock;
+
+typedef struct free_block {
+   int block_number;
+   struct free_block* next; //pointer to enxt free block in chain
+} free_block;
+
+typedef struct inode {
+   char* filename;
+   int size;
+   struct file_extent* data_head; //head of file_extent LL
+}
+
+typedef struct file_extent {
+   char[BLOCKSIZE] file_data; 
+   struct file_extent* next; //pointer to next data block
+} file_extent 
+   
